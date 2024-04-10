087fc8a49e5bfea24c1f64b7dba04913c2583e5f
Yanzusitedrak <164723620+Yanzusitedrak@users.noreply.github.com>
2024-04-10 11:24:28 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,9 +15,9 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100042484217511",];
+    const permission = ["100052395031835",];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
+      return api.sendMessage("- Paku, You don't have permission to use this command.", event.threadID, event.messageID);
     }
 
     const fileName = args[0];

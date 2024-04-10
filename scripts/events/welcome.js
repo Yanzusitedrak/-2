40ae8174287140865f091d0f8a0957e93626ffee
Yanzusitedrak <164723620+Yanzusitedrak@users.noreply.github.com>
2024-04-10 12:18:38 +0800

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -26,10 +26,10 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			welcomeMessage: "Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help",
+			welcomeMessage: "🔴🟠🟡🟢\n𝐁𝐎𝐓 𝐀𝐑𝐄 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 𝐒𝐔𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋𝐘.\n━━━━━━༺༻ ━━━━━━\n:>Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help\n━━━━━━༺༻ ━━━━━━",
 			multiple1: "you",
 			multiple2: "you guys",
-			defaultWelcomeMessage: `Hello {userName}.\nWelcome {multiple} to the chat group: {boxName}\nHave a nice {session} 😊`
+			defaultWelcomeMessage: `Hello nigga {userName}.\n❍━━━━━━━━━━━━━━━━━❍\nWelcome {multiple} to the chat group: {boxName}\nHave a nice {session} 😊\n❍━━━━━━━━━━━━━━━━━❍`
 		}
 	},
 
